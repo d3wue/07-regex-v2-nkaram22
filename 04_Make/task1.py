7c6f4fb537dbf72c73a025d2c336d306d881b944
nkaram22 <167405722+nkaram22@users.noreply.github.com>
2024-07-11 07:19:03 +0000

    first incomplete

--- 04_Make/task1.py
+++ 04_Make/task1.py
@@ -8,3 +8,19 @@ for ip in fileHandler:
 fileHandler.close()
 
 # Your code starts here
+
+print(ipAddresses)
+
+count4 = 0
+count6 = 0
+count0 = 0
+
+reg4 = re.
+
+for ip in ipAddresses:
+    if ip == reg4:
+        count4 +=1
+    elif ip == reg6:
+        count6 +=1
+    else:
+        count= += 1
